0704f0292f1c3625e7a45f24e40f7f1c0cb3e3e2
Miguel Caravantes <miguel.caravantes0@gmail.com>
2019-01-24 22:34:30 -0600

    update years of experience

--- src/Profile.js
+++ src/Profile.js
@@ -5,7 +5,7 @@ import Section from "./Section";
 const Profile = () => {
   return (
     <Section icon={AccountCircleIcon} title="Profile">
-      Senior Developer with over 5 years of experience in the software
+      Senior Developer with over 6 years of experience in the software
       development process, with knowledge in software architecture, design
       patterns and good practices, focused in results and write clean code,
       passion for technologies, self-taught and proactive person, high ability
